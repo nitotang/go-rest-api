d7ad3d5045f2662b03a97ae7b953b04532a0cbc6
Fernando Tang <nitotang@gmail.com>
2021-06-14 22:37:44 -0500

    Added health check

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/nitotang/go-rest-api/internal/transport/http"
+)
 
 // App - the struct which contains things like
 // pointers to database connections
@@ -9,11 +14,21 @@ type App struct{}
 // Run - handles the startup of our application
 func (app *App) Run() error {
 	fmt.Println("Setting Up Our App")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set up server")
+		return err
+	}
+
 	return nil
 }
 
 // Our main entrypoint for the application
 func main() {
+	fmt.Println("Go REST API course")
 	app := App{}
 	if err := app.Run(); err != nil {
 		fmt.Println("Error Starting Up")
